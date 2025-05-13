dba76609ff1271db276f2f16ef7fe9404bc648c0
Ian Githinji <hian14550@gmail.com>
2025-05-13 19:32:36 +0300

    First Commit

---
+++ script.js
@@ -0,0 +1,65 @@
+
+alert("Welcome to PLP Web Dev February Cohort!");
+
+
+// Readmore Function
+let isExpanded = false;
+
+function toggleReadMore() {
+    const about = document.getElementById("aboutText");
+    const button = document.getElementById("readMoreBtn");
+
+    if (!isExpanded) {
+        about.innerHTML = `
+            Welcome to an exciting 8-week journey where you'll transform from a coding novice
+            to a confident front-end web developer! This hands-on, project-driven course will
+            equip you with essential skills to build stunning, responsive, and interactive web pages.
+            Get ready to learn and secure your future.
+        `;
+        button.textContent = "Read Less";
+        isExpanded = true;
+    } else {
+        about.innerHTML = `
+            Welcome to an exciting 8-week journey where you'll transform from a coding novice
+            to a confident front-end web developer!
+        `;
+        button.textContent = "Read More";
+        isExpanded = false;
+    }
+}
+
+
+//Theme Toggle Function
+let darkMode = false;
+function changeTheme() {
+    if (!darkMode) {
+        document.body.style.backgroundColor = "#222";
+        document.body.style.color = "#fff";
+        document.querySelectorAll("section").forEach(section => {
+            section.style.backgroundColor = "#333";
+            section.style.borderRadius = "8px";
+            section.style.padding = "10px";
+        });
+        darkMode = true;
+    } else {
+        document.body.style.backgroundColor = "#fff";
+        document.body.style.color = "#000";
+        
+        document.querySelectorAll("section").forEach(section => {
+            section.style.backgroundColor = "transparent";
+        });
+        darkMode = false;
+    }
+}
+
+// Show/Hide Extra Info
+function toggleExtraInfo() {
+    const extra = document.getElementById("extrainfo");
+    if (extra.style.display === "none") {
+        extra.style.display = "block";
+    } else {
+        extra.style.display = "none";
+    }
+}
+
+
